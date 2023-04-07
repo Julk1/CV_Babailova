a88f3758fd06a6cba1ab42f7e0bd3e5240dab251
Julk1 <babailova.j.b@gmail.com>
2023-04-07 17:30:31 +0200

    lesson28

--- lesson28.js
+++ lesson28.js
@@ -13,31 +13,31 @@ photo.addEventListener("click", function() {
 });
 
 
-const sectionHeader = document.querySelectorAll('.header');
-const alphabet = 'abcdefghijklmnopqrstuvwxyz';
-let letterIndex = 0;
+//const sectionHeader = document.querySelectorAll('.header');
+//const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+//let letterIndex = 0;
 
-for (let i = 0; i < sectionHeader.length; i++) {
-  const header = sectionHeader[i];
-  const letter = alphabet[letterIndex % alphabet.length].toUpperCase();
-  letterIndex++;
-  header.innerHTML = `<span class="header-letter">${letter}</span>${header.innerHTML}`;
+// for (let i = 0; i < sectionHeader.length; i++) {
+  //const header = sectionHeader[i];
+  //const letter = alphabet[letterIndex % alphabet.length].toUpperCase();
+  //letterIndex++;
+ // header.innerHTML = `<span class="header-letter">${letter}</span>${header.innerHTML}`;
 
 
-document.addEventListener('keydown', function(event) {
+//document.addEventListener('keydown', function(event) {
    
-    if (event.code === 'KeyA') {
-        const section = document.querySelector('.summary');
-        section.scrollIntoView({behavior: 'smooth'});
-    } else if (event.code === 'KeyB') {
-        const section = document.querySelector('.skills');
-        section.scrollIntoView({behavior: 'smooth'});
-    } else if (event.code === 'KeyC') {
-        const section = document.querySelector('.work');
-        section.scrollIntoView({behavior: 'smooth'});
-    } else if (event.code === 'KeyD') {
-        const section = document.querySelector('.education');
-        section.scrollIntoView({behavior: 'smooth'});
-    }
-  });
-}
+    //if (event.code === 'KeyA') {
+        //const section = document.querySelector('.summary');
+        //section.scrollIntoView({behavior: 'smooth'});
+    //} else if (event.code === 'KeyB') {
+        //const section = document.querySelector('.skills');
+        //section.scrollIntoView({behavior: 'smooth'});
+    //} else if (event.code === 'KeyC') {
+       // const section = document.querySelector('.work');
+       // section.scrollIntoView({behavior: 'smooth'});
+   // } else if (event.code === 'KeyD') {
+   //     const section = document.querySelector('.education');
+   //     section.scrollIntoView({behavior: 'smooth'});
+   // }
+ // });
+//}
