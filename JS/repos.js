eb20b014c289a2da94e498650a5a32bb3e279dbf
Julk1 <babailova.j.b@gmail.com>
2023-05-19 21:23:24 +0200

    lesson34

---
+++ JS/repos.js
@@ -0,0 +1,49 @@
+class GithubApi {
+    constructor (token, username) {
+
+        this.token = token;
+        this.username = username;
+
+    }
+
+    getRepos() {
+        const headers = new Headers ();
+        headers.append('Authorization', `Bearer ${this.token}`);
+
+        return fetch(`https://api.github.com/users/${this.username}/repos`, { headers })
+        .then(response => response.json())
+
+    }
+}
+
+const api = new GithubApi ('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' , 'Julk1') ;
+document.addEventListener('DOMContentLoaded', function () {
+    api.getRepos()
+      .then(data => {
+        const projectsSection = document.getElementById('projects-section');
+  
+        data.forEach(repo => {
+          const repoCard = document.createElement('div');
+          repoCard.classList.add('repo-card');
+          repoCard.style.border = '1px solid black';
+          repoCard.style.padding = '10px';
+          repoCard.style.marginBottom = '15px';
+  
+          const repoLink = document.createElement('a');
+          repoLink.href = repo.html_url;
+          repoLink.textContent = repo.full_name;
+          repoCard.appendChild(repoLink);
+          repoLink.style.textDecoration = 'underline';
+  
+          if (repo.description) {
+            var repoDescription = document.createElement('p');
+            repoDescription.textContent = repo.description;
+            repoCard.appendChild(repoDescription);
+            repoDescription.style.margin = '0';
+          }
+  
+          projectsSection.appendChild(repoCard);
+        });
+      })
+    
+  });
